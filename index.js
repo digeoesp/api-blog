1c174d09f0c8c05bf5966aed5db09d4dddc95776
digeoesp <diegocrypto@outlook.com>
2021-07-19 16:35:40 -0700

    models created

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const app = express();
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 
+
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(console.log("Connected to MONGODB"))
+    .catch((err) => console.log(err));
 
 
 app.listen('5000', () => {
